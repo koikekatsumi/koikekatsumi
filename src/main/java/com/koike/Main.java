14225328cd1366141d2fa9e2b6d06067eae705fb
koikekatsumi <katsumikoike724@gmail.com>
2024-03-23 23:36:08 +0900

    Hokutonokenのプログラムの作成

---
+++ src/main/java/com/koike/Main.java
@@ -0,0 +1,14 @@
+package com.koike;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args ) {
+             System.out.println("あああ");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("たっ" + i +"発");
+            if (i==100)
+                System.out.println("おわったーっ、北斗百烈拳、お前は、もう既に死んでいる。ひでぶー" );
+        }
+    }
+}
